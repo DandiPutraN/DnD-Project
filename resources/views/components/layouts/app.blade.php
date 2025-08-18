77a3d166bdd506020323b922cd30eed538b6bac3
DandiPutraN <dandiputran@gmail.com>
2025-08-18 14:00:47 +0700

    update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,40 +4,52 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
+
         <title>Dandi Putra Nugraha Portfolio</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('front/images/Logo.png') }}" />
-    
+
+        <!-- Filament CSS -->
+        @filamentStyles
+
+        <!-- Vite CSS -->
+        @vite('resources/css/app.css')
+
         <!-- Custom CSS -->
         <link rel="stylesheet" href="{{ asset('front/style.css') }}" />
-    
+
         <!-- Box Icons -->
         <link
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         />
-      </head>
+    </head>
 
     <body>
         @livewire('partials.navbar')
+
         <main>
             {{ $slot }}
         </main>
+
         @livewire('partials.footer')
+
+        <!-- Filament JS -->
+        @filamentScripts
+
         @livewireScripts
+
+        <!-- Typed.js -->
+        <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+        <!-- Email.js -->
+        <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+        <script>
+          (function () {
+            emailjs.init("tkKxnFTTTampSyr3G");
+          })();
+        </script>
+
+        <!-- Custom JS -->
+        <script src="{{ asset('front/script.js') }}" defer></script>
     </body>
-    <!-- Typed.js -->
-    <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
-
-    <!-- Email.js -->
-    <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
-    <script>
-      (function () {
-        emailjs.init("tkKxnFTTTampSyr3G");
-      })();
-    </script>
-
-    <!-- Custom js -->
-    <script src="{{ asset('front/script.js') }}" defer></script>
-
 </html>
