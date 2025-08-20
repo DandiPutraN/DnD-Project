676cdcade08c26bf062d61fe03dcfdc5958fe8a8
DandiPutraN <dandiputran@gmail.com>
2025-08-20 13:35:45 +0700

    updates repository apk

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,30 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function ($middleware) {
+        // Debugging setiap request
+        $middleware->append(function (\Illuminate\Http\Request $request, \Closure $next) {
+            Log::info('Request Debug', [
+                'url' => $request->fullUrl(),
+                'method' => $request->method(),
+                'headers' => $request->headers->all(),
+                'cookies' => $request->cookies->all(),
+                'session' => session()->all(),
+            ]);
+
+            return $next($request);
+        });
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->withExceptions(function ($exceptions) {
+        // bisa ditambahkan custom exception handler di sini
+    })
+    ->create();
